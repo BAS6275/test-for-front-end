9f3f2130e2101dd9b06c27735be7f8b5076c9bb8
BAS6275 <bas6275@gmail.com>
2020-08-19 14:52:03 +0700

    edit overhull F3

--- src/views/Function-3.vue
+++ src/views/Function-3.vue
@@ -1,44 +1,54 @@
 <template>
   <div id="function3">
-    <div class="d-flex justify-center">
-      <div class="mb-8">
-        <!-- <h2>Numbers = {{ number }}</h2> -->
+    <div id="div1" class="mt-8 d-flex justify-center">
+      <div id="div2" class="mt-2 setWidth">
+        <v-container grid-list-xs>
+          <v-row>
+            <v-col cols="6">
+              <v-btn class="mr-4" @click="clear" color="error">Clear</v-btn>
+              <v-btn class="mr-4" @click="findMax2nd">Find second max</v-btn>
+              <v-text-field
+                v-for="(n, index) in numberOfInput"
+                :key="index"
+                type="number"
+                placeholder="input number "
+                v-model.lazy="number[index]"
+              >
+              </v-text-field>
+            </v-col>
 
-        <v-text-field
+            <v-col cols="6">
+              <div class="d-flex justify-center">
+                <v-btn color="info" @click="numberOfInput += 1"
+                  >Add a number</v-btn
+                >
+              </div>
+              <div v-if="isActive">
+                List of numbers :
+                <span v-for="(n, index) in number" :key="index"> {{ n }}</span>
+              </div>
+              <v-card
+                class="d-flex align-center justify-center pa-4 mx-auto"
+                max-width="550"
+                min-height="76"
+                outlined
+                v-if="isActive"
+              >
+                <div>Max2nd = {{ max2nd }}</div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+        <!-- <v-text-field
+          max="10"
+          min="1"
+          label="Main input"
           type="number"
-          placeholder="input number 1"
-          v-model="number[0]"
-        ></v-text-field>
-        <v-text-field
-          type="number"
-          placeholder="input number 2"
-          v-model="number[1]"
-        ></v-text-field>
-        <v-text-field
-          type="number"
-          placeholder="input number 3"
-          v-model="number[2]"
-        ></v-text-field>
-        <v-text-field
-          type="number"
-          placeholder="input number 4"
-          v-model="number[3]"
-        ></v-text-field>
-        <v-text-field
-          type="number"
-          placeholder="input number 5"
-          v-model="number[4]"
-        ></v-text-field>
-        <v-text-field
-          type="number"
-          placeholder="input number 6"
-          v-model="number[5]"
-        ></v-text-field>
-        <h2 v-if="displayResult">Second Max = {{ max }}</h2>
-        <v-btn class="mr-4" @click="findMax2(), (displayResult = true)"
-          >Find Second Max</v-btn
-        >
-        <v-btn class="mr-4" @click="clear">Clear</v-btn>
+          name="numberOfInput"
+          v-model.number="numberOfInput"
+          :rules="rules"
+          @input="test($event)"
+        ></v-text-field> -->
       </div>
     </div>
   </div>
@@ -46,13 +56,19 @@
 
 <script>
 export default {
-  name: "function3",
   data() {
     return {
       number: [],
       sortedNum: [],
-      max: "",
-      displayResult: false,
+      max: 0,
+      max2nd: null,
+      isActive: false,
+      //ตัว Input
+      numberOfInput: 1,
+      // rules: [
+      //   (value) => !!value || "Required.",
+      //   (value) => (value && value <= 10) || "max 10 ",
+      // ],
     };
   },
   methods: {
@@ -60,21 +76,44 @@ export default {
       this.sortedNum = this.number.sort(function(a, b) {
         return a - b;
       });
+      console.log(this.sortedNum);
     },
-    findMax2() {
+    findMax() {
       this.sortedArray();
-      if (this.sortedNum.length > 1) {
-        this.max = this.sortedNum[this.sortedNum.length - 2];
-      } else if (this.sortedNum.length == 1) {
-        this.max = this.sortedNum;
-      } else {
-        this.max = "null";
+      // this.max = this.sortedNum[this.sortedNum.length - 1];
+      this.max = Math.max(...this.sortedNum);
+      console.log("ค่าแม็ก" + this.max);
+    },
+    findMax2nd() {
+      this.sortedArray();
+      this.findMax();
+      this.isActive = true;
+
+      for (let i = this.sortedNum.length; i >= 0; i--) {
+        if (this.max > this.sortedNum[i]) {
+          this.max2nd = this.sortedNum[i];
+          break;
+        } else if (this.sortedNum.length == 1) {
+          this.max2nd = this.sortedNum[i];
+        } else {
+          this.max2nd = "null";
+        }
+        // else if (this.sortedNum[i] == )
       }
-      console.log(this.max);
+      console.log("ค่าแม็กที่2 " + this.max2nd);
+    },
+
+    test() {
+      console.log(typeof this.numberOfInput);
     },
     clear() {
       this.number = [];
-      this.displayResult = false;
+      this.sortedNum = [];
+      this.max = 0;
+      this.max2nd = null;
+      this.isActive = false;
+      //ตัว Input
+      this.numberOfInput = 1;
     },
   },
 };
